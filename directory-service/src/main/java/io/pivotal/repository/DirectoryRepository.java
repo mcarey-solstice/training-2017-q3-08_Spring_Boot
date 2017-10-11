4802c842e4a1f282833b71f491c3dbe63f797913
Mike Carey <mcarey@solstice.com>
2017-10-10 19:56:15 -0500

    Directory Service

---
+++ directory-service/src/main/java/io/pivotal/repository/DirectoryRepository.java
@@ -0,0 +1,8 @@
+package io.pivotal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import io.pivotal.domain.Directory;
+
+public interface DirectoryRepository extends CrudRepository<Directory, Long> {
+
+}
